84b668464c338e7fa1f50598b8c3aed21d972c1c
krotoselqui <dragon0549610@yahoo.co.jp>
2025-02-12 12:15:37 +0900

    get access key, fixed keypair ref names

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "key_pair_name" {
-    description = ""
+    description = "Name of the key pair used for EC2 instance SSH access. The private key file (terraform-webapp.pem) should be in the project directory."
     type = string
+    default = "terraform-webapp"
 }
